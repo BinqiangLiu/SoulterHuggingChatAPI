ced662cd6d7370bb4ce88abdf5c8ba6463016a13
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-10 11:10:47 +0800

    Create app4render.py

---
+++ app4render.py
@@ -0,0 +1,42 @@
+#https://github.com/Soulter/hugging-chat-api
+import streamlit as st
+from hugchat import hugchat
+from hugchat.login import Login
+import tiktoken
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+# Log in to huggingface and grant authorization to huggingchat
+account_name = st.text_input("Enter your HF account name:")
+account_pwd = st.text_input("Enter your HF account password:")
+
+sign = Login(account_name, account_pwd)
+cookies = sign.login()
+
+# Save cookies to usercookies/<email>.json
+sign.saveCookiesToDir()
+
+chat_resp = ""
+
+# Create a ChatBot
+chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+
+# Create a new conversation
+id = chatbot.new_conversation()
+chatbot.change_conversation(id)
+
+while True:
+    user_input = input("What would you like me to answer? (Enter 'exit' to quit): ")
+    if user_input.lower() == 'exit':
+        break
+    
+    chat_resp = chatbot.chat(user_input)
+    print("\n"+"Your input: "+user_input)
+    print("\n"+"AI Response:"+"\n"+chat_resp)
+
+    tokens = encoding.encode(chat_resp)
+    token_count=len(tokens)
+    print("\n"+"Token counts: "+str(token_count)+"\n")
+
+# Get conversation list
+conversation_list = chatbot.get_conversation_list()
+print("App Exited. Conversation list IDs: " + str(conversation_list))
