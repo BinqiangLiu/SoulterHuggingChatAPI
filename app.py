5b840a2e75cb461fce5e67e1d2fbb623796e5797
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-10 11:58:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ sign = Login(account_name, account_pwd)
 cookies = sign.login()
 
 # Save cookies to usercookies/<email>.json
-sign.saveCookiesToDir()
+sign.saveCookies()
+#sign.saveCookiesToDir()
 
 chat_resp = ""
 
@@ -42,5 +43,5 @@ while True:
 
 # Get conversation list
 conversation_list = chatbot.get_conversation_list()
-st.wreite("App Exited. Conversation list IDs: " + str(conversation_list))
+st.write("App Exited. Conversation list IDs: " + str(conversation_list))
 print("App Exited. Conversation list IDs: " + str(conversation_list))
