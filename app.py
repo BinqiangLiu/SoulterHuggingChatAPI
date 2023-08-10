96e8159e82d0f0a1131021ecb25de16a98d15ce4
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-08-10 11:26:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #https://github.com/Soulter/hugging-chat-api
-
+import streamlit as st
 from hugchat import hugchat
 from hugchat.login import Login
 import tiktoken
 encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
 # Log in to huggingface and grant authorization to huggingchat
-sign = Login(input("Please enter your account name: "), input("Please enter your account password: "))
+account_name = st.text_input("Enter your HF account name:")
+account_pwd = st.text_input("Enter your HF account password:")
+
+sign = Login(account_name, account_pwd)
 cookies = sign.login()
 
 # Save cookies to usercookies/<email>.json
@@ -22,18 +25,22 @@ id = chatbot.new_conversation()
 chatbot.change_conversation(id)
 
 while True:
-    user_input = input("What would you like me to answer? (Enter 'exit' to quit): ")
+    user_input = st.text_input("What would you like me to answer? (Enter 'exit' to quit): ")
     if user_input.lower() == 'exit':
         break
     
     chat_resp = chatbot.chat(user_input)
+    st.write("\n"+"Your input: "+user_input)
+    st.write("\n"+"AI Response:"+"\n"+chat_resp)
     print("\n"+"Your input: "+user_input)
     print("\n"+"AI Response:"+"\n"+chat_resp)
 
     tokens = encoding.encode(chat_resp)
     token_count=len(tokens)
+    st.write("\n"+"Token counts: "+str(token_count)+"\n")
     print("\n"+"Token counts: "+str(token_count)+"\n")
 
 # Get conversation list
 conversation_list = chatbot.get_conversation_list()
+st.wreite("App Exited. Conversation list IDs: " + str(conversation_list))
 print("App Exited. Conversation list IDs: " + str(conversation_list))
